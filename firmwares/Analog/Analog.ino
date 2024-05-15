af8e8b5f6a84494235280273e41411df14decb54
maidnl <d.aimo@arduino.cc>
2024-05-15 10:06:08 +0200

    removed debug unused from analog firmware

--- firmwares/Analog/Analog.ino
+++ firmwares/Analog/Analog.ino
@@ -25,28 +25,7 @@ void setup() {
   }
 
   oa = (OptaAnalog *)OptaExpansion;
-
 }
-//#define DEBUG_ANALOG_FW
-#ifdef DEBUG_ANALOG_FW
-void debug_task() {
-  static unsigned long int start = millis();
-  if(millis() -  start > 10000) {
-    start = millis();
-    oa->displayOaDebugInformation();
-    /*
-    for(int i = 0; i < 8; i++) {
-      Serial.print("   Channel ");
-      Serial.print(i);
-      Serial.print(" RTD value ");
-      Serial.println(oa->getRtdValue(i));
-    }
-    */
-    
-  } 
-}
-#endif
-
 
 
 /* -------------------------------------------------------------------------- */
@@ -56,10 +35,5 @@ void loop() {
 /* -------------------------------------------------------------------------- */
   if(OptaExpansion != nullptr) {
     OptaExpansion->update();
-#ifdef DEBUG_ANALOG_FW
-    debug_task();
-#endif
-
   }
-
 }
